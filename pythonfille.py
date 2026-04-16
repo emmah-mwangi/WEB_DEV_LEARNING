ebe09ba23721bd6818df938efc88f3d41589d4df
Mwangi2006 <mwangiemmah2023@gmail.com>
2026-04-16 08:31:17 +0300

    Initial commit

---
+++ pythonfille.py
@@ -0,0 +1,15 @@
+numbers = [ 1,2,3,4,5]
+print(numbers[0])
+print(numbers[1])
+print(numbers[2])
+print(numbers[3])
+print(numbers[4])
+
+for i in numbers:
+    print (i)
+if 8 in numbers:
+    print("8 is in the list")
+else:
+    print("8 is NOT in the list")
+print(numbers[0])
+print(numbers[4])
